ecf049b0c98428611315bad4f80d07a46b1037a9
Kuril951 <viodle238@naver.com>
2018-05-18 06:08:00 +0900

    (F)파일 합치기

--- ACM-ICPC/Asia Regional/예선/Daejeon Nationalwide Internet Competition 2015/(F)파일 합치기.cpp
+++ ACM-ICPC/Asia Regional/예선/Daejeon Nationalwide Internet Competition 2015/(F)파일 합치기.cpp
@@ -1,51 +1,47 @@
 #include<iostream>
 #include<algorithm>
-#include<memory.h>
+#include<cstring>
+#include<climits>
 
 using namespace std;
 
-int file[501];
-int dp[501][501];
+int N, book[1000];
+int dp[1000][1000];
 
-int merge_file(int s, int e)
+int func(int s, int e)
 {
-	if (s == e)
+	if (s + 1 == e)
 		return 0;
 
 	int &ret = dp[s][e];
-
-	if (ret != 0)
+	if (ret)
 		return ret;
+	ret = INT_MAX;
 
-	ret += 999999999;
 	int sum = 0;
-
-	for (int i = s; i <= e ; i++)
-		sum += file[i];
-
 	for (int i = s; i < e; i++)
-		ret = min((merge_file(s, i) + merge_file(i + 1, e)) + sum, ret);
+		sum += book[i];
+	for (int i = s + 1; i < e; i++)
+		ret = min(ret, func(s, i) + func(i, e) + sum);
 
 	return ret;
 }
 
 int main()
 {
-	int T, K;
+	int T;
 
 	scanf("%d", &T);
 
 	while (T--)
 	{
-		int sum = 0;
-		scanf("%d", &K);
-
 		memset(dp, 0, sizeof(dp));
+		scanf("%d", &N);
 
-		for (int i = 0; i < K; i++)
-			scanf("%d", &file[i]);
+		for (int i = 0; i < N; i++)
+			scanf("%d", &book[i]);
 
-		printf("%d\n", merge_file(0,K-1));
+		printf("%d\n", func(0, N));
 	}
 
 	return 0;
